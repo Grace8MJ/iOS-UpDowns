fd70f50533432073fdea18e82f582c825c3ce2b5
Minji Choi <gracemj805@gmail.com>
2021-02-15 17:02:56 +0900

    Initial Commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by Minji on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
